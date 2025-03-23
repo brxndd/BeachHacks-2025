b6b60b51b5603c2fdfa0be2d56f0938ae8dd52ec
WTL04 <williamluu04@gmail.com>
2025-03-22 20:25:05 -0700

    added prototype homepage and fixing local host errors by increasing mem limit

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image
-            src="/images/Heart2Heart.png"
+            src="/images/mini_logo.png"
             alt="Logo"
             width={32}
             height={32}
