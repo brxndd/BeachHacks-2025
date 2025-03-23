f8dec3d8ebcaf2064d4fe99bfd873f57ecf1dc23
WTL04 <williamluu04@gmail.com>
2025-03-22 21:40:44 -0700

    hero homepage finished for frontend, no functionality so far

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,35 @@
 import Image from "next/image";
-import Questionnaire from "../components/Questionnaire";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Questionnaire from "../components/Questionnaire";
 
 export default function Home() {
-
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
       <main className="flex-grow">
         <div className="container mx-auto px-4 py-8">
-          <Image
-              src="/images/Heart2Heart_white.png"
+          <div className="flex justify-center">
+            <Image
+              src="/images/Heart.png"
               alt="Logo"
-              width={50}
-              height={50}
-              className="h-8"
+              width={150}  // Smaller default size
+              height={150}  // Smaller default size
+              className="object-contain mt-32 md:mt-40 lg:mt-48 md:w-48 md:h-48 lg:w-56 lg:h-56"
             />
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          </div>
+
+
+          <div className="text-[#CA0808] flex justify-center items-center flex-col">
+            <h1 className="text-5xl font-bold mt-8 md:text-6xl lg:text-7xl">HEART2HEART </h1>
+            <p className="text-xl mb-32 italic">Small Steps, Big Impact</p>
+          </div>
+
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
             <div className="bg-white p-6 rounded-lg shadow-lg">
-              <h2 className="text-2xl font-semibold text-[#CA0808]">Home</h2>
-              <p className="mt-2 text-gray-600">Explore our services and resources.</p>
+              <h2 className="text-2xl font-semibold text-[#CA0808]">Daily Checklist</h2>
+              <p className="mt-2 text-gray-600">Explore our services and re</p>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-lg">
               <h2 className="text-2xl font-semibold text-[#CA0808]">Chatbot</h2>
@@ -30,10 +39,6 @@ export default function Home() {
               <h2 className="text-2xl font-semibold text-[#CA0808]">Tracker</h2>
               <p className="mt-2 text-gray-600">Track your health metrics with ease.</p>
             </div>
-          </div>
-          <div className="text-center mt-8 space-x-4">
-            <button className="bg-[#CA0808] text-white px-6 py-2 rounded-lg hover:bg-red-800">Sign Up</button>
-            <button className="bg-[#CA0808] text-white px-6 py-2 rounded-lg hover:bg-red-800">Sign In</button>
           </div>
         </div>
       </main>
