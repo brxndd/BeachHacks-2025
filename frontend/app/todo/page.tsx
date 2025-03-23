2e52a9bc99823408ac14de7ca42b75ddd6bd8d9a
Brandon Chu <brand.c168@gmail.com>
2025-03-23 02:06:07 -0700

    Added missing navigation bar

--- frontend/app/todo/page.tsx
+++ frontend/app/todo/page.tsx
@@ -1,8 +1,12 @@
+'use client'
 import Questionnaire from "../../components/Questionnaire"
-
+import NavBar from "../../components/NavBar"
 
 export default function TodoList() {
   return (
-    <Questionnaire />
+    <div>
+      <NavBar />
+      <Questionnaire />
+    </div>
   );
 }
